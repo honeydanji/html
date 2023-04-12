bc368bea5a395a602eb8dcac356cb0db9c31f1f5
honeydanji <tjdwls3680@naver.com>
2023-04-12 11:19:32 +0900

    HTML 수업 05(html.css.js)

---
+++ 05/script/16.js
@@ -0,0 +1,29 @@
+const show = () => {
+    event.preventDefault() ;
+
+    //랜덤수 생성
+    let n = Math.floor(Math.random() * 6) + 1;
+    document.querySelector(".h2Class").innerHTML = `<img src='../img/${n}.png'>`; // innerHTML에 해당하는 경로가 기준이다.
+
+    //사용자 입력 수 : 라디오의 체크값을 찾음
+    let use; // user 변수
+    const radios = document.querySelectorAll('input[type=radio]'); // 태그를 찾아온다. []를 사용하면 속성값을 찾을 수 있다.
+    for(let item of radios) {
+        console.log(item);
+        if (item.checked){
+            user = item.Value; break;
+        }
+    }
+
+    
+    //랜덤수와 사용자입력수 같은지 검사
+    if (n === parseInt(user)) {
+        document.querySelector('article h1').textContent = '주사위게임 : 일치(승)'
+    }
+    else {
+        document.querySelector('article h1').textContent = '주사위게임 : 불일치(패)'
+    }
+
+
+    
+}
